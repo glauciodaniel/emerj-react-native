dee36712727f6ead53970c599e98f7932ec09eb0
Glaucio Daniel <glauciodaniel@gmail.com>
2022-01-31 20:05:50 -0300

    Deploy

--- components/HomeItemsComponents/index.tsx
+++ components/HomeItemsComponents/index.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import { Text, View } from '../Themed';
+import styled from 'styled-components';
+
+const BackgroundContainer = styled.View`
+    height: 100%;
+    width: 100%;
+    background: #1e2d3eee;
+    position: relative;
+`
+const ImageBackground = styled.Image`
+    height: 100%;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    opacity: 0.5;
+`
+
+const ContentItems = styled.View`
+position: absolute;
+width: 100%;
+min-height: 100%;
+`
 
 export default function HomeItemsComponents (){
     return (
         <>
-            <View>
-                <Text>Home</Text>
-            </View>
+            <BackgroundContainer>
+                <ContentItems>
+                    <Text>Home</Text>
+                </ContentItems>
+                <ImageBackground source={require('../../assets/images/background.jpg')}/>
+            </BackgroundContainer>
         </>
     )
 }
